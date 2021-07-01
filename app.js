34b040b7f264e7932a648dec4528a43ec21c51fc
ssarvesh18 <ssarvesh.kumar1899@gmail.com>
2021-07-01 13:15:41 +0530

    oops, forgot a step

--- app.js
+++ app.js
@@ -28,9 +28,14 @@ const port = process.env.PORT||5000;
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static("project/build"));
+    
 }
 const server = app.listen(port,function(err){
     console.log(`running on port : ${port}`);
+    const path = require("path");
+    app.get("*", (req, res) => { 
+        res.sendFile(path.resolve(__dirname,'project','build','index.html'));
+    })
 });
 
 const io = socket(server,{cors:"*"});
